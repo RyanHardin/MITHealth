0ac41dda3228480c781e678c4541fbd86e52a834
RyanHardin <rhardin23.rh@gmail.com>
2020-02-29 00:12:39 -0500

    addition to the login

--- screens/Login.js
+++ screens/Login.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, Button, StyleSheet, TextInput} from 'react-native';
 
 const Login = ({navigation}) => {
+  const [user, setUser] = useState ('');
+  const [passWord, setPassword] = useState ('');
+
+  const login = () => {
+    console.log (`The username/Email: ${user}`);
+    console.log (`The password: ${passWord}`);
+    navigation.navigate ('Home');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.greeting}>{`Hello Again.\nWelcome back`}</Text>
-      <TextInput style={styles.input} placeholder="Username/Email" />
-      <TextInput style={styles.input} placeholder="Password" />
-      <Button title={'Login'} onPress={() => navigation.navigate ('Home')} />
-      <Button title={'Sign Up'} onPress={() => console.log ('Working')} />
+      <View style={{width: 300}}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username/Email"
+          onChangeText={text => setUser (text)}
+          value={user}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={value => setPassword (value)}
+          value={passWord}
+        />
+      </View>
+      <Button title={'Login'} onPress={login} />
+      <Button title={'Sign Up'} onPress={() => console.log ()} />
     </View>
   );
 };
@@ -31,6 +53,7 @@ const styles = StyleSheet.create ({
   input: {
     height: 40,
     borderBottomColor: 'grey',
+    borderBottomWidth: 0.5,
   },
 });
 
