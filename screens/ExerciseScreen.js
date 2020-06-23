f2bbb90b240ac7364a143f4269a83bd4537a0695
Ryan Hardin <rhardin23.rh@gmail.com>
2020-06-22 23:03:00 -0400

    starting over

--- screens/ExerciseScreen.js
+++ screens/ExerciseScreen.js
@@ -1,73 +1,42 @@
-import React from 'react';
-import {View, StyleSheet, Text, Image, TouchableOpacity} from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
+import { Card } from "react-native-material-ui";
 
 export default function ExerciseScreen() {
+  const images = [
+    require("./../assets/images/airhockey.png"),
+    require("./../assets/images/pool.jpeg"),
+    require("./../assets/images/Blocks.png"),
+    require("./../assets/images/pingpong.png"),
+    require("./../assets/images/drumming.png"),
+    require("./../assets/images/basketball.png"),
+  ];
+
+  const cards = images.map((icon, i) => (
+    <View key={i} style={styles.exercise}>
+      <TouchableOpacity style={styles.exercise}>
+        <Image style={{ width: 150, height: 150 }} source={icon} />
+      </TouchableOpacity>
+    </View>
+  ));
+
   return (
     <View style={styles.container}>
-      <View style={styles.exContainer}>
-        <TouchableOpacity style={styles.exercise}>
-          <Image
-            style={{width: 150, height: 150}}
-            source={require('./../assets/images/airhockey.png')}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.exercise}>
-          <Image
-            style={{width: 150, height: 150}}
-            source={require('./../assets/images/pool.jpeg')}
-          />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.exContainer}>
-        <TouchableOpacity style={styles.exercise}>
-          <Image
-            style={{width: 150, height: 150}}
-            source={require('./../assets/images/Blocks.png')}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.exercise}>
-          <Image
-            style={{width: 150, height: 150}}
-            source={require('./../assets/images/pingpong.png')}
-          />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.exContainer}>
-        <TouchableOpacity style={styles.exercise}>
-          <Image
-            style={{width: 150, height: 150}}
-            source={require('./../assets/images/drumming.png')}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.exercise}>
-          <Image
-            style={{
-              width: 150,
-              height: 150,
-            }}
-            source={require('./../assets/images/basketball.png')}
-          />
-        </TouchableOpacity>
-      </View>
+      <Text>Exercise</Text>
     </View>
   );
 }
 
 ExerciseScreen.navigationOptions = {
-  title: 'Exercises',
+  title: "Exercises",
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-evenly',
+    justifyContent: "space-between",
     paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-
-  exContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
+    backgroundColor: "#fff",
   },
   exercise: {
     width: 150,
--- screens/Login.js
+++ screens/Login.js
@@ -1,59 +1,48 @@
-import React, {useState} from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
-import {Button} from 'react-native-elements';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Button } from "react-native";
 
-const Login = ({navigation}) => {
-  const [user, setUser] = useState ('');
-  const [passWord, setPassword] = useState ('');
-  const [error, setError] = useState ('');
+const Login = ({ navigation }) => {
+  const [user, setUser] = useState("");
+  const [passWord, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const login = () => {
-    if (user != '' && passWord != '') {
-      navigation.navigate ('Home');
+    if (user != "" && passWord != "") {
+      navigation.navigate("Home");
     } else {
-      setError ('Please insert correct usename or password');
+      setError("Please insert correct usename or password");
     }
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.greeting}>{`Hello Again,\nWelcome back`}</Text>
-      <Text style={{padding: 15, color: 'red'}}>{error}</Text>
-      <View style={{width: 400}}>
+      <Text style={{ padding: 15, color: "red" }}>{error}</Text>
+      <View style={{ width: 400 }}>
         <TextInput
           style={styles.input}
           placeholder="Username/Email"
-          onChangeText={text => setUser (text)}
+          onChangeText={(text) => setUser(text)}
           value={user}
         />
         <TextInput
           style={styles.input}
           placeholder="Password"
           secureTextEntry={true}
-          onChangeText={value => setPassword (value)}
+          onChangeText={(value) => setPassword(value)}
           value={passWord}
         />
       </View>
-      <Button
-        title={'Sign In'}
-        onPress={login}
-        style={{width: 350, padding: 5}}
-      />
+      <Button title={"Sign In"} onPress={login} style={{ width: 350, padding: 5 }} />
 
-      <TouchableOpacity style={{padding: 10}}>
+      <TouchableOpacity style={{ padding: 10 }}>
         <Text>
           New to Vulcan Grip?
           <Text
-            style={{fontWeight: 'bold', color: 'rgb(41, 139, 217)'}}
-            onPress={() => navigation.navigate ('Register')}
-          >
-            {' '} Sign Up
+            style={{ fontWeight: "bold", color: "rgb(41, 139, 217)" }}
+            onPress={() => navigation.navigate("Register")}>
+            {" "}
+            Sign Up
           </Text>
         </Text>
       </TouchableOpacity>
@@ -62,25 +51,25 @@ const Login = ({navigation}) => {
 };
 
 Login.navigationOptions = {
-  title: '',
+  title: "",
 };
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 50,
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    alignItems: "center",
+    backgroundColor: "#fff",
   },
   greeting: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 20,
   },
   input: {
     height: 40,
     margin: 20,
     paddingBottom: 10,
-    borderBottomColor: 'grey',
+    borderBottomColor: "grey",
     borderBottomWidth: 0.5,
   },
 });
